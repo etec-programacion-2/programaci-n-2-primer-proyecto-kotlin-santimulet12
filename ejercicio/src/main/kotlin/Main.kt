9d3096fef45b9767dabeb6a2f7c55e5f9a24dce7
santimulet12 <santiagomulet2008@gmail.com>
2025-04-22 12:12:57 -0300

    Paso 2: Variables y tipos de datos

--- ejercicio/src/main/kotlin/Main.kt
+++ ejercicio/src/main/kotlin/Main.kt
@@ -4,6 +4,10 @@ fun nombreFecha(nombre:String, fecha:String ):String {
     return "soy $nombre y hoy es $fecha"
 }
 
+fun areaRectangulo(base:Double,altura:Double):Double{
+    return base*altura
+}
+
 fun main() {
     val name:String = "Santiago"
     val surname:String = "Mulet"
@@ -21,4 +25,5 @@ fun main() {
     println("Altura: $height")
     println("Es estudiante: $esEstudiante")
 
+    println("el area de un rectangulo de base 2 y altura 5 es ${areaRectangulo(2.0,5.0)}m2")
 }
